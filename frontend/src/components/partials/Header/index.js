201ee9816c51ad36b5516bd96f9c799ddee0b873
Matheus Duarte <matheusduartegalvao@gmail.com>
2020-03-17 22:17:52 -0300

    Ajustes

--- frontend/src/components/partials/Header/index.js
+++ frontend/src/components/partials/Header/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { HeaderArea } from './styled';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <HeaderArea>
-            ...
+            <div className="container">
+                <div className="logo">
+                    <Link to="/">
+                        <span className="logo-1">O</span>
+                        <span className="logo-2">L</span>
+                        <span className="logo-3">X</span>
+                    </Link>
+                </div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="">Cadastrar</Link>
+                        </li>
+                        <li>
+                            <Link to="" className="button">Poste um anúncio</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </HeaderArea>
     );
 }
